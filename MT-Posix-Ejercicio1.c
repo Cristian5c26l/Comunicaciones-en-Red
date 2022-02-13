b7a00310ae65fad6e7bda8cf02344a137f369945
Cristian <cristian5464@outlook.com>
2022-02-12 20:07:35 -0600

    Incorporación del Ejercicio 1 de Hilos POSIX

---
+++ MT-Posix-Ejercicio1.c
@@ -0,0 +1,45 @@
+// Este programa genera n hilos especificados por el usuario utilizando memoria estática
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// Flujo de instrucciones asociado al hilo creado (hilo en cuestión)
+void *routine( void* anything ){
+    printf("Hola mundo! Soy el hilo %ld\n",pthread_self());
+}
+
+int main(int argc, char const *argv[]){
+    
+    // Comprobar el número de argumentos especificados por el usuario 
+    if( argc != 2 ){
+        printf("Falta especificar el numero de hilos en la ejecución de '\'%s!\n",argv[0]);
+        exit( 1 );
+    }
+
+    // Recuperación de los nHilos del usuario
+    int nThreads = atoi( argv[1] );
+    
+    // Arreglo de hilos
+    pthread_t thArray[nThreads];
+
+    /* Inicio de la región de concurrencia/paralelismo */
+    // Creación y ejecución de los hilos
+    for (int i = 0; i < nThreads; i++){
+        if( pthread_create( &thArray[i], NULL, routine, NULL ) < 0 ){
+            printf( "El sistema operativo no pudo crear un hilo POSIX de usuario\n" );
+            exit( 1 );
+        }
+    }
+    /* Fin de la región de concurrencia/paralelismo */
+
+    // Esperar la terminación de los hilos
+    for (int i = 0; i < nThreads; i++){
+        pthread_join( thArray[i], NULL );
+    }
+
+    printf("Fin del proceso padre!\n");
+
+    return 0;
+}
