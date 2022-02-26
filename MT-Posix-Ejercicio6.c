e1bab858b7da0c15c4a9a9482fa3eaa507e12427
Cristian <cristian5464@outlook.com>
2022-02-25 18:45:27 -0600

    Optimizacion del retorno del hilo

--- MT-Posix-Ejercicio6.c
+++ MT-Posix-Ejercicio6.c
@@ -6,47 +6,61 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <ctype.h>// Biblioteca para convertir letras minusculas a mayusculas y viceversa
 
-char const *fileName = "TestFile.txt";
+char const fileName[20] = "TestFile.txt";
 
 typedef struct{// la estructura (que se envia al hilo) contiene
     char testFile[20];// nombre del archivo donde se va a buscar la palabra
-    char testWord[100];// palabra a buscar
+    char testWord[20];// palabra a buscar
     int repetitionsNumber;// veces que fue encontrada la palabra
     long pThId;// id del hilo
 }Struct;
 
-void *routine(void* aStruct){//Flujo de instrucciones de un hilo. Funcion se convierte en un hilo
-    Struct *thStruct = (Struct*) aStruct;
-    FILE* fp = fopen(thStruct->testFile,"r");// leer el nombre del archivo que se quiere analizar con este hilo
+char* getLowerString(char*);
+void checkMallocError(void*);
+
+void *wordSearch(void* word){//Flujo de instrucciones de un hilo. Funcion se convierte en un hilo
+    char w[20];
+    strcpy(w,(char*)word);//paso de lo que esta en argv[i + 1] (que es una palabra) a w
+    //printf("WORD: %s\n",w);
+    Struct *returnData = (Struct*) malloc(sizeof(Struct));
+    checkMallocError((void*) returnData );
+    // Inicializacion de informacion de estructura a enviar al padre
+    strcpy(returnData->testFile,fileName);
+    //printf("%s\n",argv[i + 1]);
+    strcpy(returnData->testWord,getLowerString(w));
+    returnData->repetitionsNumber= 0;
+    FILE* fp = fopen(returnData->testFile,"r");// leer el nombre del archivo que se quiere analizar con este hilo
     if(fp != NULL){
 
         // Algoritmo para encontrar el numero de veces que aparece la palabra thStruct->strFind en el archivo .txt especificado
         char thWordF[100];
         char *tok; //token donde se guarda una palabra o cadena de caracteres con significado coheerente. En este caso, en tok se guardaran caracteres alfanumericos (para formar palabras o cantidades numericas) hasta que encuentre un espacio, coma, punto o algun signo de puntuacion extra
+        char *thLowerWordF;// cadena thWordF convertida en minusculas
         while(!feof(fp)){ // Recorrer todo el archivo
             // Extraer palabra por palabra del archivo
             strcpy(thWordF,"");
             fscanf(fp,"%s",thWordF);// almacenar en thWordF la palabra del archivo
-
+            thLowerWordF = getLowerString(thWordF);// convertirla a minusculas
             tok = NULL;
-            tok = strtok(thWordF," (),.:;-_!@?|#$\0\"\'\t\n\v");//strtok se encarga de extraer caracteres alfanumericos (que se pueda encontrar en thWordF) hasta que se encuentre un espacio o un ( o un ) o un . o un , ... y dichos caracteres se almacenan en tok. En teoria, tok tendria una palabra
+            tok = strtok(thLowerWordF," (),.:;-_!@?¡¿|#$\0\"\'\t\n\v");//strtok se encarga de extraer caracteres alfanumericos (que se pueda encontrar en thWordF) hasta que se encuentre un espacio o un ( o un ) o un . o un , ... y dichos caracteres se almacenan en tok. En teoria, tok tendria una palabra
 
             while(tok != NULL){//mientras tok (palabra de thWordF, que se extrae de el archivo) no sea nula 
-                if ( strcmp(tok,thStruct->testWord) == 0 ){
-                    thStruct->repetitionsNumber = thStruct->repetitionsNumber + 1;
+                if ( strcmp(tok,returnData->testWord) == 0 ){
+                    returnData->repetitionsNumber = returnData->repetitionsNumber + 1;
                 }
-                tok = strtok(NULL," (),.:;-_!@?|#$\0\"\'\t\n\v");//obten la siguiente palabra que pueda formarse con strtok utilizando la misma thWordF
+                tok = strtok(NULL," (),.:;-_!@?¡¿|#$\0\"\'\t\n\v");//obten la siguiente palabra que pueda formarse con strtok utilizando la misma thWordF
             }
             
         }
         
         // Fin de algoritmo
-        thStruct->pThId = pthread_self();
-        printf("FIN DEL HILO %ld\n", thStruct->pThId);
-        return (void*) thStruct;
+        returnData->pThId = pthread_self();
+        //printf("FIN DEL HILO %ld\n", returnData->pThId);
+        return (void*) returnData;
     }else{
-        printf("El archivo %s no se puede localizar en el directorio especificado!!\n",thStruct->testFile);
+        printf("El archivo %s no se puede localizar en el directorio especificado!!\n",returnData->testFile);
         exit(1);
     }
 }
@@ -64,46 +78,56 @@ int main(int argc, char const *argv[]){
 
     // Reservación de memoria dinámica (en el heap)
     pthread_t* thArray = ( pthread_t* )malloc( nThreads*sizeof(pthread_t) );
-    Struct *structs = (Struct*)malloc(sizeof(Struct)*nThreads);
-    Struct *thDataRet= (Struct*)malloc(sizeof(Struct)*nThreads);
-
-    // Inicializacion de informacion de estructuras a enviar a los hilos
-    for (int i = 0; i < nThreads; i++){
-        strcpy(structs[i].testFile,fileName);
-        //printf("%s\n",argv[i + 1]);
-        strcpy(structs[i].testWord,argv[i + 1]);//paso de lo que esta en argv[i + 1] (que es una palabra) a structs[i].testWord
-        structs[i].repetitionsNumber= 0;
-    }
+    checkMallocError((void*) thArray );
 
     // Creación de hilos y ejecución
     for (int i = 0; i < nThreads; i++){
         
-        if( pthread_create( &thArray[i], NULL, routine, (void*) &structs[i]) < 0 ){//paso de estructura al hilo
+        if( pthread_create( &thArray[i], NULL, wordSearch, (void*) argv[i+1]) < 0 ){//paso de estructura al hilo
             printf("El sistema operativo no pudo crear un hilo POSIX de usuario\n");
             exit( 1 );
         }
     }
 
     // Esperar la terminación de los hilos y recuperar lo que retornan a través de aux
-    Struct *aux;
+    Struct *thDataRet;
     for (int i = 0; i < nThreads; i++){
-        pthread_join( thArray[i], (void**) &aux );
-        thDataRet[i] = *aux;
-    }
-
-    // Resultados del numero de veces que aparece cada palabra
-    printf("\nDatos de retorno de los hilos\n\n");
-    for (int i = 0; i < nThreads; i++){
-        printf("Hilo %ld determinó que la palabra \"%s\" se repite %d veces en el archivo llamado %s\n",thDataRet[i].pThId,thDataRet[i].testWord,thDataRet[i].repetitionsNumber,thDataRet[i].testFile);
+        pthread_join( thArray[i], (void**) &thDataRet );
+        // Resultados del numero de veces que aparece cada palabra
+        printf("\nDatos de retorno de hilos\n\n");
+        printf("Hilo %ld determinó que la palabra \"%s\" se repite %d veces en el archivo llamado %s\n",thDataRet->pThId,thDataRet->testWord,thDataRet->repetitionsNumber,thDataRet->testFile);
+        printf("\n---------------------------------------------------------------\n");
+        free(thDataRet);
     }
     
     //Liberamos espacio de memoria dinamica
     free(thArray);
-    free(structs);
-    free(thDataRet);
     printf("\nFin del proceso padre!\n");
     
     return 0;
+}
+
+char* getLowerString(char* word){// Pasa todas las letras mayusculas que esten en una palabra a minusculas
+    int size = strlen(word);
+    char* lowerString = (char*)malloc(sizeof(char)*size);
+    checkMallocError((void*) lowerString );
+    int i = 0;
+    while (word[i] != '\0'){
+        lowerString[i] = tolower(word[i]);
+        i++;
+    }
+    
+    lowerString[i] = '\0';
+
+    return lowerString;
+    
+}
+
+void checkMallocError(void* pointer ){
+    if( pointer == NULL ){
+        printf("Error en Malloc. No se pudo generar memoria dinamica.\n");
+        exit(1);
+    }
 }
 
 //Compilacion -> gcc MT-Posix-Ejercicio6.c -lpthread
